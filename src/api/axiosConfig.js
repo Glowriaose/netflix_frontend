58984de761cc883d29363919e4c8a389f2ec07b7
Glowria.Glowria <glowriaose@gmail.com>
2026-05-19 16:17:13 +0100

    implement nginx reverse proxy for api routing

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,11 +1,19 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   // base url for local deployment on my pc
+//   // baseURL: "http://localhost:8080",
+
+//   // backend EC2 deployment (from .env)
+//   baseURL: process.env.REACT_APP_API_URL
+// });
+
+// export default api;
+
 import axios from "axios";
 
 const api = axios.create({
-  // base url for local deployment on my pc
-  // baseURL: "http://localhost:8080",
-
-  // backend EC2 deployment (from .env)
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: "/api"  // No IPs, no ports, completely dynamic!
 });
 
 export default api;
